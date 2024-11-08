633cd0f3da9e6267fbd472ce4b0bac934516e5a8
bhanu2199 <69838024+bhanu2199@users.noreply.github.com>
2024-11-08 10:52:54 +0530

    Delete Backend Microservices directory

--- Backend Microservices/coupon/src/main/java/com/deals/coupon/Repository/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package com.deals.coupon.Repository;
-
-import com.deals.coupon.Model.Coupon;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CouponRepository extends MongoRepository<Coupon,Integer> {
-}
--- Backend Microservices/customer-service/src/main/java/com/coupon/customerservice/Repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coupon.customerservice.Repository;
-
-import com.coupon.customerservice.Model.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer,String> {
-}
